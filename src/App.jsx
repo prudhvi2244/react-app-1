b7771e2f8b7e6f4c734f1d76e726a1249e2cf5bc
Advith <raj.rajeev2244@gmail.com>
2026-02-18 23:53:02 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Welcome To React JS</h1>
-      <h1>Welcome To React JS</h1>
+      <h1>{new Date().toLocaleTimeString()}</h1>
       <h1>Welcome To React JS</h1>
       <h1>Welcome To React JS</h1>
     </div>
