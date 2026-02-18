1a8e8d780e4e07b10b68ca067ad37db970b44d49
Advith <raj.rajeev2244@gmail.com>
2026-02-18 23:27:01 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome To React JS</h1>
+      <h1>Welcome To React JS</h1>
+      <h1>Welcome To React JS</h1>
+      <h1>Welcome To React JS</h1>
+    </div>
+  );
+}
+
+export default App;
